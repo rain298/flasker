48c0b9e38f792cd588bc58ded39f7f147a1f6339
dakui298@sina.com <rain298@qq.com>
2016-09-18 23:53:28 +0800

    second commit

--- config.py
+++ config.py
@@ -66,6 +66,19 @@ class ProductionConfig(Config):
         app.logger.addHandler(mail_handler)
 
 
+class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # 输出到 stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
