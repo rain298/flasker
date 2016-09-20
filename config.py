dae83fb5dc481057d115475ee6e83747f22f0421
dakui298@sina.com <rain298@qq.com>
2016-09-20 14:16:51 +0800

    successful

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config:
     MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 25
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'rain298@163.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'rain298'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'rain298@163.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'rain298@qq.com'
