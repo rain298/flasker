c2b0a7529c0c6b23990fcdaa95a970f9ddfdbc18
dakui298@sina.com <rain298@qq.com>
2016-09-20 15:16:19 +0800

    smtp

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     MAIL_PORT = 25
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'rain298@163.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'rain298'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'dota123'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'rain298@163.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'rain298@qq.com'
