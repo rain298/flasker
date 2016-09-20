52fda381dca4b1d59f2aa7a6429f4b67ce776640
dakui298@sina.com <rain298@qq.com>
2016-09-20 17:02:55 +0800

    heheeh

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
     MAIL_SERVER = 'smtp.qq.com'
-    MAIL_PORT = 465
+    MAIL_PORT = 25
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = 'rain298@qq.com'
+    MAIL_PASSWORD = 'xqitgvaclczfcbcc'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'rain298@qq.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'rain298@qq.com'
